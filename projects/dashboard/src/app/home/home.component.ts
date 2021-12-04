9e9847abe3db71000eda34ca47ca6d7da19723c5
Maksym Mostovyi <maksym.mostovyi.ua@gmail.com>
2021-12-04 16:23:50 +0400

    implemented dashboard template

--- projects/dashboard/src/app/home/home.component.ts
+++ projects/dashboard/src/app/home/home.component.ts
@@ -12,4 +12,12 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openNav() {
+    document.getElementById("mySidenav")!.style.width = "250px";
+  }
+
+  closeNav() {
+    document.getElementById("mySidenav")!.style.width = "0";
+  }
+
 }
